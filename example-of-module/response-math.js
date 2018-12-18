ad50327cc42840683ab4443ed93334f50c95730a
Santiago Cujaban <santiero12@gmail.com>
2018-12-18 17:50:29 -0500

    Use of a module

---
+++ example-of-module/response-math.js
@@ -0,0 +1,15 @@
+const matematica = require("./request-math.js");
+
+console.log(matematica);
+
+console.log("suma");
+console.log(matematica.suma(1, 2));
+
+console.log("resta");
+console.log(matematica.resta(3, 1));
+
+console.log("multiplicación");
+console.log(matematica.multiplicacion(30, 2));
+
+console.log("división");
+console.log(matematica.division(14, 0));
